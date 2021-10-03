2f8339fafce563389978fdd7459dd614d11d6d47
Aaron Wright <aaronjohnwright@gmail.com>
2021-10-03 16:56:16 +0100

    Some Updates

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,5 +1,14 @@
-from myapp import app  
+from flask import render_template, request
+from myapp import app, mongo
 
-@app.route("/")
+
+
+@app.route("/", methods = ['GET', 'POST'])
 def index():
-    return "<h1>Hello World</h1>"
+    # Our database
+    todo_collection = mongo.db.ToDoCollection
+    todos = todo_collection.find({})
+    if request.method == 'POST':
+        if request.form['content'] != "":
+            todo_collection.insert_one({"text" : request.form['content'], "complete" : False})
+    return render_template("index.html", todos=todos)
